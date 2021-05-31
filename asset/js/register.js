8d29b35730430351d98fa70a3bad7c5e77b538aa
CVEY <clement.vey@ynov.com>
2021-05-31 17:14:15 +0200

    Verification error input

--- asset/js/register.js
+++ asset/js/register.js
@@ -3,94 +3,134 @@ const Username = document.getElementById("Username__input")
 const Password = document.getElementById("Password__input")
 const Password_Verif = document.getElementById("Password_Verif__input")
 const Submit_btn = document.getElementsByClassName("submit")
+const Disclaimer_Error = document.getElementsByClassName("Disclaimer_error")
 
 Email.addEventListener("input", (event)=>{
     let email_value = Email.value
+    let error_encounter = Email_Verification(email_value)
     if (email_value == ""){
         Email.style.borderColor = "red";
-    }else if(!Email_Verification(email_value)){
+        Disclaimer_Error[0].innerHTML = "Invalide Email"
+    }else if(error_encounter.length != 0){
+        Disclaimer_Error[0].innerHTML = ""
         Email.style.borderColor = "red";
+        error_encounter.forEach(elem =>{
+            Disclaimer_Error[0].innerHTML += elem +"<br>"
+        })
     }else{
         Email.style.borderColor = "green";
+        Disclaimer_Error[0].innerHTML = ""
     }
 })
 
 function Email_Verification (email){
-    let good = true
     let double_mail = 0
-
+    const input_err = []
     email.split("").forEach((element, index)=> {
         if (element == " "){
-            good = false
+            if (input_err.includes("Space character not allowed") == false){
+                input_err.push("Space character not allowed")
+            }
         }
         if (element == "@"){
             double_mail ++
-
         }
-        if (index > 1){
+        if (index > 0){
             if (element == "." && email[index -1] == "."){
-                good = false
+                if (input_err.includes(". after an other .") == false){
+                    input_err.push(". after an other .")
+                }
+            }
+            if (element == "." && email[index -1] == "@"){
+                if (input_err.includes(". after an @") == false){
+                    input_err.push(". after an @")
+                }
             }
             if (element == "@" && email[index -1] == "."){
-                good = false
+                if (input_err.includes(". before an @") == false){
+                    input_err.push(". before an @")
+                }
             }
         }
     })
-    if (double_mail != 1 ){
-        good = false
+    if (double_mail > 1 ){
+        if (input_err.includes("To many @") == false){
+            input_err.push("To many @")
+        }
+    }else if (double_mail < 1 ){
+        if (input_err.includes("Missing @") == false){
+            input_err.push("Missing @")
+        }
     }
-    return good
+    return input_err
 }
 
 Username.addEventListener("input", (event)=>{
     let username_value = Username.value
-
-    if(username_value.length < 4 || username_value.length > 12){
+    let error_encounter = Username_Verification(username_value)
+    if (error_encounter.length != 0){
+        Disclaimer_Error[1].innerHTML = ""
         Username.style.borderColor = "red";
+        error_encounter.forEach(elem =>{
+            Disclaimer_Error[1].innerHTML += elem +"<br>"
+        })
     }else{
-        if (!Username_Verification(username_value)){
-            Username.style.borderColor = "red";
-        }else{
-            Username.style.borderColor = "green";
-        }
+        Username.style.borderColor = "green";
+        Disclaimer_Error[1].innerHTML = ""
     }
 })
 
 function Username_Verification (user){
-    let good = true
-    
+    const input_err = []
+    if(user.length < 4){
+        if (input_err.includes("Not enough characters") == false){
+            input_err.push("Not enough characters")
+        }
+    }else if (user.length > 12){
+        if (input_err.includes("Too many characters") == false){
+            input_err.push("Too many characters")
+        }
+    }
     user.split("").forEach((element, index)=> {
         if (element == " "){
-            good = false
+            if (input_err.includes("Space character not allowed") == false){
+                input_err.push("Space character not allowed")
+            }
         }
     })
-    return good
+    return input_err
 }
 
 Password.addEventListener("input", (event)=>{
     let pwd_value = Password.value
-
-    if(pwd_value.length < 6){
+    let error_encounter = Password_Verification(pwd_value)
+    if (error_encounter.length != 0){
+        Disclaimer_Error[2].innerHTML = ""
         Password.style.borderColor = "red";
+        error_encounter.forEach(elem =>{
+            Disclaimer_Error[2].innerHTML += elem +"<br>"
+        })
     }else{
-        if (!Password_Verification(pwd_value)){
-            Password.style.borderColor = "red";
-        }else{
-            Password.style.borderColor = "green";
-        }
+        Password.style.borderColor = "green";
     }
 })
 
 function Password_Verification (user){
-    let good = true
+    const input_err = []
     let Lower = 0
     let Upper = 0
     let Nb = 0
-    
+    if(user.length < 6){
+        if (input_err.includes("Not enough characters") == false){
+            input_err.push("Not enough characters")
+        }
+    }
     user.split("").forEach((element, index)=> {
         let ascii = element.charCodeAt(0)
         if (element == " "){
-            good = false
+            if (input_err.includes("Space character not allowed") == false){
+                input_err.push("Space character not allowed")
+            }
         }
         if (ascii >= 65 && ascii <= 90){
             Upper ++
@@ -100,11 +140,22 @@ function Password_Verification (user){
             Nb ++
         }
     })
-    console.log(Lower, Upper, Nb)
-    if (Lower < 1 || Upper < 1 || Nb < 1){
-        good = false
+    if (Lower < 1){
+        if (input_err.includes("Not enough lowercase") == false){
+            input_err.push("Not enough lowercase")
+        }
     }
-    return good
+    if (Upper < 1){
+        if (input_err.includes("Not enough uppsercase") == false){
+            input_err.push("Not enough uppsercase")
+        }
+    }
+    if (Nb < 1){
+        if (input_err.includes("Not enough number") == false){
+            input_err.push("Not enough number")
+        }
+    }
+    return input_err
 }
 
 Password_Verif.addEventListener("input", (event)=>{
@@ -113,8 +164,10 @@ Password_Verif.addEventListener("input", (event)=>{
 
     if(pwd_verif_value != pwd_value ){
         Password_Verif.style.borderColor = "red";
+        Disclaimer_Error[3].innerHTML = "Invalide Password"
     }else{
         Password_Verif.style.borderColor = "green";
+        Disclaimer_Error[3].innerHTML = ""
     }
 })
 
