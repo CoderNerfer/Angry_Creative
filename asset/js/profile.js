9d8f837bf34da891471f501995ce629edd1dbb7d
CVEY <clement.vey@ynov.com>
2021-06-02 13:03:26 +0200

    profilte second party responsive and Js to do

---
+++ asset/js/profile.js
@@ -0,0 +1,6 @@
+const Btn_Edit = document.getElementById("Edit_profil_btn")
+const Overview = document.getElementById("Overview")
+
+Btn_Edit.addEventListener("click", ()=>{
+    Overview.style.display = "none"
+})
