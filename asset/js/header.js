bdc568ae5fed53d3f980c0c182073c40c621dd25
CVEY <clement.vey@ynov.com>
2021-06-04 12:56:59 +0200

    header finit

--- asset/js/header.js
+++ asset/js/header.js
@@ -16,7 +16,14 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
-// document.cookie = "Login ='{'user':'Clem','mail':'mail@cookie.com','nb_posts':'10','nb_likes':'15'}'"
+document.getElementsByClassName("Login")[0].addEventListener("click", (event)=>{
+    document.getElementById("Pop_up").style.display = "block"
+    event.stopPropagation()
+})
+document.addEventListener("click", ()=>{
+    document.getElementById("Pop_up").style.display = "none"
+})
+document.cookie = "Login ='{'user':'Clem','mail':'mail@cookie.com','nb_posts':'10','nb_likes':'15'}'"
 let my_cookie_header = Select_Login_cookie()
 if (my_cookie_header != ""){
     my_cookie_header = JSON.parse(Cookie_cooker(my_cookie_header))
@@ -24,10 +31,17 @@ if (my_cookie_header != ""){
     if (my_cookie_header.user != ""){
         document.getElementsByClassName("Login")[0].setAttribute("src", "https://img.icons8.com/fluent-systems-regular/45/000000/user-male-circle.png")
         document.getElementById("Post_add").style.display = "flex"
+        document.getElementById("First_pop").innerHTML = "Profile"
+        document.getElementById("First_pop").setAttribute("href","/profile")
+        document.getElementById("Second_pop").innerHTML = "Logout"
+        document.getElementById("Second_pop").setAttribute("href","/")
     }
 }else{
     document.getElementsByClassName("Login")[0].setAttribute("src", "https://img.icons8.com/windows/50/000000/user-ninja.png")
-    document.getElementById("Post_add").style.display = "none"
+    document.getElementById("First_pop").innerHTML = "Register"
+    document.getElementById("First_pop").setAttribute("href","/register")
+    document.getElementById("Second_pop").innerHTML = "Login"
+    document.getElementById("Second_pop").setAttribute("href","/login")
 }
 
 function Select_Login_cookie (){
