911df7c9a2a4d835205e0ae8895b719c3a56d5b0
TMEIGNIER <tanguy.meignier@ynov.com>
2021-06-14 13:04:40 +0200

    page view-theme finie

--- asset/js/view-theme.js
+++ asset/js/view-theme.js
@@ -2,18 +2,18 @@ const arrowTop = document.querySelectorAll(".arrowTop")[0]
 const arrowBottom = document.querySelectorAll(".arrowBottom")[0]
 
 arrowTop.addEventListener("click", () => {
-    const theme = document.getElementById("theme")
-    theme.scrollTo({
-        top:theme.scrollTop-500,
+    const theme = document.getElementById("test")
+    test.scrollTo({
+        top:test.scrollTop-500,
         left:0,
         behavior:"smooth"
     })
 })
 
 arrowBottom.addEventListener("click", () => {
-    const theme = document.getElementById("theme")
-    theme.scrollTo({
-        top:theme.scrollTop+500,
+    const theme = document.getElementById("test")
+    test.scrollTo({
+        top:test.scrollTop+500,
         left:0,
         behavior:"smooth"
     })
